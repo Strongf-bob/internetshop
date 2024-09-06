c40bdfe7fd241e18625d7b5b615f8d32d23bb274
Strongf-bob <karsakovillya@gmail.com>
2024-09-06 22:59:27 +0300

    Add files via upload

---
+++ InternetShop2-master/internetshop/shop/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from . import models
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+# Register your models here.
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Review, ReviewAdmin)
---
+++ InternetShop2-master/internetshop/shop/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+   name = models.CharField(max_length=128)
+   description = models.TextField()
+   price = models.IntegerField()
+   image_url = models.CharField(max_length=256)
+   model = models.CharField(max_length=64, null=True, blank=True)
+   video_memory = models.FloatField(max_length=16, null=True, blank=True)
+   pages_count = models.IntegerField(max_length=96, null=True, blank=True)
+   cores_count = models.IntegerField(max_length=16, null=True, blank=True)
+
+   def __str__(self):
+      return f'{self.name}'
+
+class Review(models.Model):
+ product = models.ForeignKey(Product, on_delete=models.CASCADE)
+ author = models.CharField(max_length=256)
+ rating = models.IntegerField()
+ text = models.TextField()
+
+def __str__(self):
+ return f'{self.author}: {self.text[:100]}'
